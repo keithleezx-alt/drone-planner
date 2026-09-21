8b6517eb000d9d00536830bf7e099dfae43f4038
Brian <bxb31415@gmail.com>
2026-09-21 18:39:13 +0800

    Edited hello.py

--- src/hello.py
+++ src/hello.py
@@ -8,10 +8,11 @@ TODO (Week 1): make it print your name and today's date.
 Hint: `from datetime import date` and `date.today()`.
 """
 
-
+from datetime import date
 def main():
     # TODO: replace this line
-    print("hello, world")
+    print(date.today())
+    print("Brian")
 
 
 if __name__ == "__main__":
